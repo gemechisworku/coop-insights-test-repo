e6633b95bfede0b35c22be1ade213c42d04884c6
gemechisw <game.worku@gmail.com>
2025-03-14 16:35:02 +0300

    fixed error

--- db_handler.py
+++ db_handler.py
@@ -1,41 +1,41 @@
-from sqlalchemy import create_engine, text, inspect
+# from sqlalchemy import create_engine, text, inspect
 
-database = "demo_bank_db"
-username = "root"
-password = "root"
-host = "localhost"
-port = "3306"
-db_uri = f"mysql+pymysql://{username}:{password}@{host}:{port}/{database}"
+# database = "demo_bank_db"
+# username = "root"
+# password = "root"
+# host = "localhost"
+# port = "3306"
+# db_uri = f"mysql+pymysql://{username}:{password}@{host}:{port}/{database}"
 
-engine = create_engine(db_uri)
+# engine = create_engine(db_uri)
 
-with engine.connect() as con:
-    rows = con.execute(text("""SELECT * FROM accounts"""))
+# with engine.connect() as con:
+#     rows = con.execute(text("""SELECT * FROM accounts"""))
 
-def get_tables_description() -> str:
-    description = """Allows you to perform SQL queries on the table. Beware that this tool's output is a string representation of the execution output.
-    It can use the following tables:"""
+# def get_tables_description() -> str:
+#     description = """Allows you to perform SQL queries on the table. Beware that this tool's output is a string representation of the execution output.
+#     It can use the following tables:"""
 
-    inspector = inspect(engine)
-    tables = inspector.get_table_names()
-    for table in tables:
-        columns_info = [(col["name"], col["type"]) for col in inspector.get_columns(table)]
+#     inspector = inspect(engine)
+#     tables = inspector.get_table_names()
+#     for table in tables:
+#         columns_info = [(col["name"], col["type"]) for col in inspector.get_columns(table)]
 
-        table_description = f"Table '{table}':\n"
+#         table_description = f"Table '{table}':\n"
 
-        table_description += "Columns:\n" + "\n".join([f"  - {name}: {col_type}" for name, col_type in columns_info])
-        description += "\n\n" + table_description
+#         table_description += "Columns:\n" + "\n".join([f"  - {name}: {col_type}" for name, col_type in columns_info])
+#         description += "\n\n" + table_description
         
 
-    output = """
-    Args:
-        query: The query to perform. This should be correct SQL.
-    """
+#     output = """
+#     Args:
+#         query: The query to perform. This should be correct SQL.
+#     """
 
-    description += "\n\n" + output
-    return description
+#     description += "\n\n" + output
+#     return description
 
-# table_description = get_tables_description()
+# # table_description = get_tables_description()
 
 
-# print(table_description)
+# # print(table_description)
